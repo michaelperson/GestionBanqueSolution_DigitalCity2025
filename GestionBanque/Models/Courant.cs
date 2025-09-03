eaa34c712ca6e9ac1a15f06c7491ac7cb8063f34
michael person <michael.person@cognitic.be>
2025-09-03 11:16:33 +0200

    Création de la classe Epargne sans reflexion d'héritage ==> Redondance de code

--- GestionBanque/Models/Courant.cs
+++ GestionBanque/Models/Courant.cs
@@ -103,9 +103,29 @@ namespace GestionBanque.Models
             Solde += MontantD;
 
         }
-        #endregion
 
-        //Surcharge de l'opérateur +
+        #region Surcharge de l'opérateur +
+
+        /// <summary>
+        /// Opérateur + permettant d'additionner le solde de deux comptes
+        /// </summary>
+        /// <param name="c1">Compte courant N°1</param>
+        /// <param name="c2">Compte courant N°2</param>
+        /// <returns>La somme des actifs des comptes sans tenir compte des valeurs négatives</returns>
+        /// <example>
+        /// Courant c1 = new Courant();
+        /// c1.Depot(100);
+        /// Courant c2 = new Courant();
+        /// c2.Depot(200);
+        /// double sommeDesActifs = c1+c2 //Donne 300
+        /// 
+        /// Courant c1 = new Courant();
+        /// c1.Depot(100);
+        /// c1.Retrait(400);
+        /// Courant c2 = new Courant();
+        /// c2.Depot(200);
+        /// double sommeDesActifs = c1+c2 //Donne 200
+        /// </example>
         public static double operator +(Courant c1, Courant c2)
         {
             //return (c1.Solde<0?0:c1.Solde)+ (c2.Solde<0?0:c2.Solde);
@@ -118,16 +138,36 @@ namespace GestionBanque.Models
 
             return solde1 + solde2;
         }
-
+        /// <summary>
+        /// Opérateur + permettant d'additionner un double représantant un montant avec le solde d'un compte courant
+        /// </summary>
+        /// <param name="c1">Somme de départ</param>
+        /// <param name="c2">Compte courant N°2</param>
+        /// <returns>La somme du solde + la somme de départ sans tenir compte des valeurs négatives</returns>
+        /// <example>
+        /// double c1 = 100;
+        /// Courant c2 = new Courant();
+        /// c2.Depot(200);
+        /// double sommeDesActifs = c1+c2 //Donne 300
+        /// 
+        /// Courant c1 = 100;
+        /// Courant c2 = new Courant();
+        /// c.Depot(200);         
+        /// c.Retrait(400);
+        /// double sommeDesActifs = c1+c2 //Donne 100
+        /// </example>
         public static double operator +(double c1, Courant c2)
         {
-            double solde1 = c1>0?c1:0;
+            double solde1 = c1 > 0 ? c1 : 0;
             double solde2 = 0;
-             
+
             if (c2.Solde >= 0) solde2 = c2.Solde;//Si le compte positif , je prend sa valeur
 
             return solde1 + solde2;
         }
+        #endregion
+
+        #endregion
 
     }
 }
---
+++ GestionBanque/Models/Epargne.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionBanque.Models
+{
+    public class Epargne
+    {
+        #region Private fields
+        private string _numero;
+        private double _solde;
+        private DateTime _dateDernierRetrait;
+        private Personne _titulaire;
+
+        #endregion
+        #region Properties
+        public string Numero
+        {
+            get
+            {
+                return _numero;
+            }
+
+            set
+            {
+                _numero = value;
+            }
+        }
+
+        public double Solde
+        {
+            get
+            {
+                return _solde;
+            }
+            private set { _solde = value; }
+        }
+
+        public DateTime DateDernierRetrait
+        {
+            get
+            {
+                return _dateDernierRetrait;
+            }
+
+            set
+            {
+                _dateDernierRetrait = value;
+            }
+        }
+
+        public Personne Titulaire
+        {
+            get
+            {
+                return _titulaire;
+            }
+
+            set
+            {
+                _titulaire = value;
+            }
+        }
+        #endregion
+        #region Methods
+        /// <summary>
+        /// Permet de retirer un montant du solde du compte  
+        /// </summary>
+        /// <param name="MontantR">Montant positif à retirer</param>
+        /// <remarks>Un compte épargne ne peut pas être en négatif!</remarks>
+        public void Retrait(double MontantR)
+        {
+            if (MontantR <= 0)
+            {
+                Console.WriteLine("Le montant doit être  strictement positif");
+                return;
+            }
+            if (Solde  < MontantR)
+            {
+                Console.WriteLine("Votre solde est insuffisant");
+                return;
+            }
+            Solde -= MontantR;
+        }
+        /// <summary>
+        /// Procédure permettant de déposer un montant positif sur notre compte
+        /// </summary>
+        /// <param name="MontantD">Montant positif à déposer</param>
+        public void Depot(double MontantD)
+        {
+            if (MontantD <= 0)
+            {
+                Console.WriteLine("Le montant doit être strictement positif");
+                return;
+            }
+            //Solde = Solde + Montant;
+            Solde += MontantD;
+
+        }
+        #endregion
+    }
+}
